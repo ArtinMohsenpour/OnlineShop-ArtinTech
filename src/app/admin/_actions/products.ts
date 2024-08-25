53e18e57e9355f27a54b2bed1646e7231e92923f
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-08-25 21:17:13 +0200

    actions

---
+++ src/app/admin/_actions/products.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import db from "@/db/db";
+import { z } from "zod";
+import fs from "fs/promises"; //for storing file in order to have access to its path
+
+const fileSchema = z.instanceof(File, { message: "Required" });
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/")
+);
+
+// the reason we dont check size of images up in files is that they are only required in adding new product
+const addSchema = z.object({
+  name: z.string().min(2),
+  description: z.string().min(3),
+  priceInCents: z.coerce.number().int().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "Required"),
+  image: imageSchema.refine((file) => file.size > 0, "Required"),
+});
+
+export async function addProduct(formData: FormData) {
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors;
+  }
+  console.log(result);
+  const data = result.data;
+
+  await fs.mkdir("products", { recursive: true });
+  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer())); // standard way to receive a file in any format and convert it to a buffer that js can read
+
+  await fs.mkdir("public/products", { recursive: true });
+  const imagePath = `products/${crypto.randomUUID()}-${data.image.name}`;
+  await fs.writeFile(
+    `public${imagePath}`,
+    Buffer.from(await data.image.arrayBuffer())
+  ); // standard way to receive a file in any format and convert it to a buffer that js can read
+
+  await db.product.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath, // to save the file path we need to first save our file to file system before we have access to its path
+      imagePath,
+    },
+  });
+}
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
+import { addProduct } from "../../_actions/products";
 
 export function ProductForm() {
   const [priceInCents, setPriceInCents] = useState<number>();
 
   return (
-    <form className="space-y-8">
+    <form action={addProduct} className="space-y-8">
       <div className="space-y-2">
         <Label className="ml-2" htmlFor="name">
           Name
@@ -32,20 +34,29 @@ export function ProductForm() {
         <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
         </div>
-        <div className="space-y-2">
-          <Label className="ml-2" htmlFor="description">
-            Description
-          </Label>
-          <Textarea id="description" name="description" required />
-        </div>
-        <div className="space-y-2">
-          <Label className="ml-2" htmlFor="file">
-            File
-          </Label>
-          <Input type="file" id="file" name="file" required />
-        </div>
-
       </div>
+      <div className="space-y-2">
+        <Label className="ml-2" htmlFor="description">
+          Description
+        </Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label className="ml-2" htmlFor="file">
+          File
+        </Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <div className="space-y-2">
+        <Label className="ml-2" htmlFor="file">
+          Image
+        </Label>
+        <Input type="file" id="image" name="image" required />
+      </div>
+
+      <Button type="submit" className="right-0">
+        Save
+      </Button>
     </form>
   );
 }
