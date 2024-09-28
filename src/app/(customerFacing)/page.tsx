364f3c60e8963ac0122c6a51028c95382f445c08
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-09-28 20:05:26 +0200

    mahsa

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/button";
 import db from "@/db/db";
+import { Product } from "@prisma/client";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 function getMostPopularProducts() {
   return db.product.findMany({
@@ -25,8 +29,11 @@ function getNewestProducts() {
 export default function HomePage() {
   return (
     <main className="space-y-12">
-      <ProductsGridSection productsFetcher={getMostPopularProducts} />
-      <ProductsGridSection productsFetcher={getNewestProducts} />
+      <ProductsGridSection
+        title="Most Popular"
+        productsFetcher={getMostPopularProducts}
+      />
+      <ProductsGridSection title="Newest" productsFetcher={getNewestProducts} />
     </main>
   );
 }
@@ -40,5 +47,18 @@ function ProductsGridSection({
   productsFetcher,
   title,
 }: ProductsGridSectionProps) {
-  return;
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products" className="space-x-2">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+    </div>
+  );
 }
