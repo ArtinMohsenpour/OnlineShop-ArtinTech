29cb97b7789db41f477281706b846ca32c2076d8
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-09-30 12:10:05 +0200

    image components are updated

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,48 @@
-import { Card } from "./ui/card";
+import { formatCurrency } from "@/lib/formatters";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
-export function ProductsCard(){
-return <Card>
-    
-</Card>
+type ProductCardProps = {
+  id: string;
+  name: string;
+  priceInCents: number;
+  description: string;
+  imagePath: string;
+};
+
+export function ProductsCard({
+  name,
+  priceInCents,
+  description,
+  id,
+  imagePath,
+}: ProductCardProps) {
+  return (
+    <Card className="flex overflow-hidden flex-col">
+      <div className="relative w-full h-auto aspect-video ">
+        <Image src={imagePath} fill alt={name} />
+      </div>
+      <CardHeader>
+        <CardContent>{name}</CardContent>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex-grow">
+        <p className="line-clamp-4">{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Button>
+          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
